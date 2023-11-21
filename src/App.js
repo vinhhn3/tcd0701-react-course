67c8e270f8ff08afce6bd58fcd7421c92ce158a4
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-11-21 10:15:25 +0700

    Search and display Users

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 import "./App.css";
 import Search from "./components/Search";
@@ -5,13 +6,29 @@ import Users from "./components/Users";
 import Navbar from "./layout/Navbar";
 
 class App extends React.Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {}
+
+  handleSearch = (text) => {
+    console.log(text);
+    axios
+      .get(`https://api.github.com/search/users?q=${text}`)
+      .then((response) => {
+        console.log(response.data.items);
+        this.setState({ users: response.data.items });
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
-          <Users />
+          <Search handleSearch={this.handleSearch} />
+          <Users users={this.state.users} />
         </div>
       </div>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 
 export default class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault(); // Stop form loading
+    this.props.handleSearch(this.state.text);
+  };
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ text: e.target.value });
+  };
   render() {
     return (
       <div>
         <form>
-          <input type="text" placeholder="Search" />
-          <button className="btn btn-dark btn-block" type="submit">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Search"
+          />
+          <button
+            onClick={this.handleSubmit}
+            className="btn btn-dark btn-block"
+            type="submit"
+          >
             Search
           </button>
         </form>
