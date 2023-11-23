f29c97060b730b1cf2b16b915ba653483acdb931
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-11-23 11:05:09 +0700

    Change navbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar bg-primary">
-      <h2>TCD0701 Github Finder</h2>
+      <h2>This Is My FYP</h2>
       <ul>
         <li>
           <Link to="/">Home</Link>
